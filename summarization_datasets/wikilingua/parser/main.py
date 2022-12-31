b80a9deed7e6dcfbb6055b5f4dc70b38f3b66e95
vladsolovyev <vlsol90@yahoo.com>
2022-12-31 20:12:14 +0100

    Dataset parser

---
+++ summarization_datasets/wikilingua/parser/main.py
@@ -0,0 +1,26 @@
+from datasets import load_dataset
+from sentencepiece import SentencePieceProcessor
+
+data_types = ["train", "test", "validation"]
+new_data_types = ["train", "test", "valid"]
+columns = ["source", "target"]
+new_columns = ["input_text", "summary"]
+languages = ["en", "es", "ru"]
+datasets = [load_dataset("GEM/wiki_lingua", "en"),
+            load_dataset("GEM/wiki_lingua", "es"),
+            load_dataset("GEM/wiki_lingua", "ru")]
+
+
+def main():
+    spp = SentencePieceProcessor(model_file="../../mbart.cc25.v2/sentence.bpe.model")
+    for data_type, new_data_type in zip(data_types, new_data_types):
+        for column, new_column in zip(columns, new_columns):
+            for language, dataset in zip(languages, datasets):
+                encoded_texts = [" ".join(encoded_parts) for encoded_parts in spp.encode(dataset[data_type][column], out_type=str)]
+                with open("{}.{}.{}".format(new_data_type, new_column, language), "w", encoding="utf-8") as f:
+                    for encoded_text in encoded_texts:
+                        f.write("{}\n".format(encoded_text))
+
+
+if __name__ == '__main__':
+    main()
