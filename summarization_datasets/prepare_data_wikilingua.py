d71c2c058964b6415311603a21b5b56a59b74662
vladsolovyev <vlsol90@yahoo.com>
2023-05-31 18:41:12 +0200

    Update data preprocessing for wikilingua

--- summarization_datasets/prepare_data_wikilingua.py
+++ summarization_datasets/prepare_data_wikilingua.py
@@ -8,43 +8,26 @@ data_types = ["train", "sampled_validation", "test"]
 new_data_types = ["train", "validation", "test"]
 columns = ["source", "target"]
 dataset_name = "wikilingua"
-mono_languages = ["en_XX", "es_XX", "ru_RU", "de_DE"]
-crosslingual_source_languages_en = ["es_XX", "ru_RU"]
-crosslingual_source_languages_de = ["en_XX", "es_XX", "ru_RU"]
+mono_languages = ["en_XX", "es_XX", "ru_RU"]
+crosslingual_languages = ["es_XX", "ru_RU", "tr_TR"]
 datasets_monolingual = [load_dataset("GEM/wiki_lingua", "en", cache_dir="./cache"),
                         load_dataset("GEM/wiki_lingua", "es", cache_dir="./cache"),
-                        load_dataset("GEM/wiki_lingua", "ru", cache_dir="./cache"),
-                        load_dataset("GEM/wiki_lingua", "de", cache_dir="./cache")]
-datasets_crosslingual_en = [load_dataset("GEM/wiki_lingua", name="es_en", cache_dir="./cache"),
-                            load_dataset("GEM/wiki_lingua", name="ru_en", cache_dir="./cache")]
-datasets_crosslingual_de = [load_dataset("GEM/wiki_lingua", name="ru_de", cache_dir="./cache"),
-                            load_dataset("GEM/wiki_lingua", name="es_de", cache_dir="./cache"),
-                            load_dataset("GEM/wiki_lingua", name="en_de", cache_dir="./cache")]
+                        load_dataset("GEM/wiki_lingua", "ru", cache_dir="./cache")]
+datasets_crosslingual = [load_dataset("GEM/wiki_lingua", name="es_en", cache_dir="./cache"),
+                         load_dataset("GEM/wiki_lingua", name="ru_en", cache_dir="./cache"),
+                         load_dataset("GEM/wiki_lingua", name="tr_en", cache_dir="./cache")]
 spp = SentencePieceProcessor(model_file="mbart.cc25.v2/sentence.bpe.model")
 
 
 def main():
     statistics = dict()
-    for language, dataset in zip(crosslingual_source_languages_en, datasets_crosslingual_en):
+    for language, dataset in zip(crosslingual_languages, datasets_crosslingual):
         for data_type, new_data_type in zip(data_types, new_data_types):
             filtered_dataset = dataset[data_type]. \
                 filter(lambda sample: sample["target_language"] == "en" and sample["source_language"] != "en")
             statistics["{}-en_XX-{}".format(language, data_type)] = \
                 encode_dataset_and_create_statistics(dataset_name, filtered_dataset, language, "en_XX",
                                                      new_data_type, columns, add_few_shot=True)
-
-    for language, dataset in zip(crosslingual_source_languages_de, datasets_crosslingual_de):
-        for data_type, new_data_type in zip(data_types, new_data_types):
-            filtered_dataset = dataset[data_type]. \
-                filter(lambda sample: sample["target_language"] == "de" and sample["source_language"] != "de")
-            statistics["{}-de_DE-{}".format(language, data_type)] = \
-                encode_dataset_and_create_statistics(dataset_name, filtered_dataset, language,
-                                                     "de_DE", new_data_type, columns)
-            filtered_dataset = dataset[data_type]. \
-                filter(lambda sample: sample["target_language"] != "de" and sample["source_language"] == "de")
-            statistics["{}-de_DE-{}".format(language, data_type)] = \
-                encode_dataset_and_create_statistics(dataset_name, filtered_dataset, "de_DE",
-                                                     language, new_data_type, columns)
 
     for language, dataset in zip(mono_languages, datasets_monolingual):
         for data_type, new_data_type in zip(data_types[:2], new_data_types[:2]):
--- summarization_scripts/preprocess_data_wikilingua.py
+++ summarization_scripts/preprocess_data_wikilingua.py
@@ -2,38 +2,26 @@ from summarization_scripts.utils import preprocess_data
 
 
 def main():
-    for language in ["es_XX", "ru_RU"]:
-        for data_size in [10, 100, 1000, 10000]:
-            preprocess_data(language,
-                            "en_XX",
-                            "wikilingua_{}-en_XX_{}".format(language, data_size),
-                            "wikilingua_{}".format(data_size))
-            preprocess_data(language,
-                            "en_XX",
-                            "wikilingua_{}-en_XX".format(language),
-                            "wikilingua_{}".format(data_size),
-                            add_validation_data=True,
-                            add_train_data=False)
+    for language in ["es_XX", "ru_RU", "tr_TR"]:
         preprocess_data(language,
                         "en_XX",
                         "wikilingua_{}-en_XX".format(language),
                         "wikilingua",
                         add_test_data=True,
                         add_validation_data=True)
-    for language in ["en_XX", "es_XX", "ru_RU", "de_DE"]:
+        for data_size in [10, 100, 1000]:
+            preprocess_data(language,
+                            "en_XX",
+                            "wikilingua_{}-en_XX_{}".format(language, data_size),
+                            "wikilingua_{}".format(data_size))
+    for language in ["es_XX", "ru_RU"]:
+        preprocess_data(language,
+                        "en_XX",
+                        "wikilingua_{}-en_XX_{}".format(language, 10000),
+                        "wikilingua_{}".format(10000))
+    for language in ["en_XX", "es_XX", "ru_RU"]:
         preprocess_data(language, language, "wikilingua_{}-{}".format(language, language),
                         "wikilingua", add_validation_data=True)
-    for language in ["en_XX", "es_XX", "ru_RU"]:
-        preprocess_data(language,
-                        "de_DE",
-                        "wikilingua_{}-de_DE".format(language),
-                        "wikilingua",
-                        add_validation_data=True)
-        preprocess_data("de_DE",
-                        language,
-                        "wikilingua_de_DE-{}".format(language),
-                        "wikilingua",
-                        add_validation_data=True)
 
 
 if __name__ == '__main__':
