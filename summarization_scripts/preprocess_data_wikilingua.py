b9409335b02029a819f1c2d1623715e910efb2bc
vladsolovyev <vlsol90@yahoo.com>
2023-06-24 15:09:52 +0200

    Addtion to the last commit

--- summarization_scripts/preprocess_data_wikilingua.py
+++ summarization_scripts/preprocess_data_wikilingua.py
@@ -2,22 +2,23 @@ from summarization_scripts.utils import preprocess_data
 
 
 def main():
-    for language in ["es_XX", "ru_RU", "tr_TR"]:
-        preprocess_data(language,
-                        "en_XX",
-                        "wikilingua_{}-en_XX".format(language),
+    for source_language, target_language in zip(["es_XX", "ru_RU", "tr_TR", "es_XX", "en_XX", "tr_TR"],
+                                                ["en_XX", "en_XX", "en_XX", "ru_RU", "tr_TR", "tr_TR"]):
+        preprocess_data(source_language,
+                        target_language,
+                        "wikilingua_{}-{}".format(source_language, target_language),
                         "wikilingua",
                         add_test_data=True,
                         add_validation_data=True)
         for data_size in [10, 100, 1000]:
-            preprocess_data(language,
-                            "en_XX",
-                            "wikilingua_{}-en_XX_{}".format(language, data_size),
+            preprocess_data(source_language,
+                            target_language,
+                            "wikilingua_{}-{}_{}".format(source_language, target_language, data_size),
                             "wikilingua_{}".format(data_size))
-    for language in ["es_XX", "ru_RU"]:
-        preprocess_data(language,
-                        "en_XX",
-                        "wikilingua_{}-en_XX_{}".format(language, 10000),
+    for source_language, target_language in zip(["es_XX", "ru_RU", "es_XX"], ["en_XX", "en_XX", "ru_RU"]):
+        preprocess_data(source_language,
+                        target_language,
+                        "wikilingua_{}-{}_{}".format(source_language, target_language, 10000),
                         "wikilingua_{}".format(10000))
     for language in ["en_XX", "es_XX", "ru_RU"]:
         preprocess_data(language, language, "wikilingua_{}-{}".format(language, language),
