138c2fd22b0decf8d7479c493fb9965ff0145065
vladsolovyev <vlsol90@yahoo.com>
2023-11-21 12:03:28 +0100

    Don't collect all values of classification, just add them

--- fairseq/scoring/classification.py
+++ fairseq/scoring/classification.py
@@ -19,21 +19,20 @@ class ClassificationScorer(BaseScorer):
     def __init__(self, cfg):
         super(ClassificationScorer, self).__init__(cfg)
         self.cfg = cfg
-        self.classifications = None
+        self.classifications = np.array([0, 0, 0])
+        self.samples_number = 0
         self.scores = dict()
 
     def add_classification_out(self, classification_out, src_pad_idx):
-        classification_out = F.softmax(classification_out.float(), dim=-1)
-        if self.classifications is None:
-            self.classifications = np.array(classification_out[~src_pad_idx])
-        else:
-            self.classifications = np.append(self.classifications, classification_out[~src_pad_idx], 0)
+        classifications = F.softmax(classification_out.float(), dim=-1)[~src_pad_idx]
+        self.samples_number += len(classifications)
+        self.classifications = np.add(self.classifications, classifications.sum(axis=0).cpu())
 
     def score(self, order=4):
         return 0.0
 
     def result_string(self, order=4):
-        scores = np.round(self.classifications.mean(axis=0), 4)
+        scores = np.round(self.classifications / self.samples_number, 4)
         for language, score in zip(languages, scores):
             self.scores[language] = score
         return "Encoder output classification: {}".format(self.scores)
