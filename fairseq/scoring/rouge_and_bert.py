0e93d59269feeabafcbaf7792d2aeac79f047689
vladsolovyev <vlsol90@yahoo.com>
2024-02-19 11:47:47 +0100

    Rouge and Bert scores with intervals

--- fairseq/scoring/rouge_and_bert.py
+++ fairseq/scoring/rouge_and_bert.py
@@ -10,6 +10,7 @@ import numpy as np
 import torch
 from langid.langid import LanguageIdentifier, model
 from rouge_score import rouge_scorer
+from rouge_score.scoring import BootstrapAggregator
 from tqdm import tqdm
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
@@ -64,12 +65,8 @@ class RougeBertScoreScorer(BaseScorer):
         rouge_result_with_stemming = dict()
         rouge_result_without_stemming = dict()
         if self.cfg.rouge_scorer == "huggingface":
-            rouge_result_with_stemming = evaluate.load("rouge", cache_dir="./cache").compute(predictions=self.pred,
-                                                                                             references=self.ref,
-                                                                                             use_stemmer=True)
-            rouge_result_without_stemming = evaluate.load("rouge", cache_dir="./cache").compute(predictions=self.pred,
-                                                                                                references=self.ref,
-                                                                                                use_stemmer=False)
+            rouge_result_with_stemming = self.calculate_huggingface_rouge_scores(use_stemmer=True)
+            rouge_result_without_stemming = self.calculate_huggingface_rouge_scores(use_stemmer=False)
         elif self.cfg.rouge_scorer == "multilingual":
             rouge_result_with_stemming = self.calculate_multilingual_rouge_scores(use_stemmer=True)
             rouge_result_without_stemming = self.calculate_multilingual_rouge_scores(use_stemmer=False)
@@ -79,13 +76,34 @@ class RougeBertScoreScorer(BaseScorer):
                                          rouge_result_without_stemming.items()}
         return rouge_result_with_stemming | rouge_result_without_stemming
 
+    def calculate_huggingface_rouge_scores(self, use_stemmer=False):
+        fmeasure_rouge = evaluate.load("rouge", cache_dir="./cache").\
+            compute(predictions=self.pred, references=self.ref, use_stemmer=use_stemmer, use_aggregator=False)
+        aggregator = BootstrapAggregator()
+        for values in zip(*fmeasure_rouge.values()):
+            aggregator.add_scores(dict(zip(fmeasure_rouge, values)))
+        aggregator_result = aggregator.aggregate()
+        results = dict()
+        for key, value in aggregator_result.items():
+            for level, value_per_level in zip(value._fields, value):
+                results["{}_{}".format(key, level)] = value_per_level
+        return results
+
     def calculate_multilingual_rouge_scores(self, use_stemmer=False):
         scorer = rouge_scorer.RougeScorer(["rouge1", "rouge2", "rougeL", "rougeLsum"],
                                           use_stemmer=use_stemmer,
                                           lang=mbart_lang_to_rouge_lang[self.cfg.lang])
         scores_all_samples = [scorer.score(pred, ref) for pred, ref in zip(self.pred, self.ref)]
-        return {metric: np.mean([score_per_sample[metric] for score_per_sample in scores_all_samples])
-                for metric in scores_all_samples[0]}
+        results = {metric: np.mean([score_per_sample[metric] for score_per_sample in scores_all_samples])
+                   for metric in scores_all_samples[0]}
+        aggregator = BootstrapAggregator()
+        for scores in scores_all_samples:
+            aggregator.add_scores(scores)
+        aggregator_result = aggregator.aggregate()
+        for key, value in aggregator_result.items():
+            for level, value_per_level in zip(value._fields, value):
+                results["{}_{}".format(key, level)] = value_per_level.fmeasure
+        return results
 
     def calculate_language_probabilities(self):
         identifier = LanguageIdentifier.from_modelstring(model, norm_probs=True)
@@ -99,7 +117,15 @@ class RougeBertScoreScorer(BaseScorer):
                                                                               model_type="bert-base-multilingual-cased")
         if bert_result["hashcode"]:
             del bert_result["hashcode"]
-        return {"bert_score_{}".format(k): np.mean(v) for k, v in bert_result.items()}
+        aggregator = BootstrapAggregator()
+        for values in zip(*bert_result.values()):
+            aggregator.add_scores(dict(zip(bert_result, values)))
+        aggregator_result = aggregator.aggregate()
+        results = dict()
+        for key, value in aggregator_result.items():
+            for level, value_per_level in zip(value._fields, value):
+                results["bert_score_{}_{}".format(key, level)] = value_per_level
+        return results
 
     def rouge_and_bert_score(self):
         print("number of samples: {}".format(len(self.pred)))
@@ -110,7 +136,8 @@ class RougeBertScoreScorer(BaseScorer):
                 sentences = nltk.sent_tokenize(text)
                 inputs = tokenizer(sentences, return_tensors="pt", truncation=True, padding=True).to(device)
                 translated_tokens = nllb_model.generate(**inputs,
-                                                        forced_bos_token_id=tokenizer.lang_code_to_id[lang_to_nllb[self.cfg.translate_to_lang]],
+                                                        forced_bos_token_id=tokenizer.lang_code_to_id[
+                                                            lang_to_nllb[self.cfg.translate_to_lang]],
                                                         max_new_tokens=200)
                 translated.append(" ".join(tokenizer.batch_decode(translated_tokens, skip_special_tokens=True)))
             self.pred = translated
